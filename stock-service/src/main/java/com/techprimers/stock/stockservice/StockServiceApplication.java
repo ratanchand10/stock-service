ba5dd379029550ad91f92a6d24fb27870865e8ea
ratanchand10 <ratanchandya10@gmail.com>
2019-05-22 20:09:13 +0530

    stock commit

--- stock-service/src/main/java/com/techprimers/stock/stockservice/StockServiceApplication.java
+++ stock-service/src/main/java/com/techprimers/stock/stockservice/StockServiceApplication.java
@@ -2,12 +2,13 @@ package com.techprimers.stock.stockservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class StockServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockServiceApplication.class, args);
 	}
-
 }
